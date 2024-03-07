cf4b26ac5c453e96d5afac8b5889a68cacc27f69
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2024-03-07 21:53:22 +0200

    updated according to the extra tests

--- syntax_analyzer.py
+++ syntax_analyzer.py
@@ -52,6 +52,11 @@ def check_assignment_syntax(tokens):
         error = ERRORS["incorrect_assignment"]
         return success, error
 
+    if tokens[0].type not in VALID_START or tokens[-1].type not in VALID_END:
+        success = False
+        error = ERRORS["invalid_start"]
+        return success, error
+    
     expression_tokens = tokens[2:]
     return check_expression_syntax(expression_tokens)
 
@@ -151,15 +156,21 @@ if __name__ == "__main__":
     success8, error8 = check_assignment_syntax(get_tokens("x + y"))
     success9, error9 = check_assignment_syntax(get_tokens("x ="))
     success10, error10 = check_assignment_syntax(get_tokens("x = (a+b)"))
-
+    success11, error11 = check_assignment_syntax(get_tokens("x = (a = b)"))
+    success12, error12 = check_assignment_syntax(get_tokens("_sdf = ="))
+    success13, error13 = check_expression_syntax(get_tokens("x = x"))
+    
     assert not success1 and error1 == 'Неправильно розставлені дужки'
     assert not success2 and error2 == "Недопустима пара токенів Token(type='operation', value='*'), Token(type='operation', value='/')" 
-    assert not success3 and not success4
-    assert not success5 and error5 == "Недопустима пара токенів Token(type='operation', value='-'), Token(type='other', value='.')"
-    assert not success6 and error6 == "Порожній вираз"
-    assert success7 and error7 == ""
-    assert not success8 and error8 == "Неправильне присвоєння"
+    assert not success3 and not success4 
+    assert not success5 and error5 == "Недопустима пара токенів Token(type='operation', value='-'), Token(type='other', value='.')" 
+    assert not success6 and error6 == "Порожній вираз" 
+    assert success7 and error7 == "" 
+    assert not success8 and error8 == "Неправильне присвоєння" 
     assert not success9 and error9 == "Порожній вираз"
     assert success10 and error10 == ""
+    assert not success11 and error11 == "Недопустима пара токенів Token(type='variable', value='a'), Token(type='equal', value='=')"
+    assert not success12 and error12 == "Недопустимий початок або кінець"
+    assert not success13 and error13 == "Недопустима пара токенів Token(type='variable', value='x'), Token(type='equal', value='=')"
 
     print("Success =", True)
